cc540bb3dfb2c4fa92e9797df6d6d6026caf5393
Brandao-GBL <qwp16274@gmail.com>
2025-08-08 03:11:42 -0300

    ULTIMO

--- Banco/ConexaoPostgres.java
+++ Banco/ConexaoPostgres.java
@@ -3,22 +3,18 @@ package Banco;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.Properties;
 public class ConexaoPostgres {
-
-    private static final String URL = "jdbc:postgresql://localhost:5432/comercio";
-    private static final String USUARIO = "admin";
-    private static final String SENHA = "0109";
-
-    static {
+    private static final String URL = "jdbc:postgresql://localhost:5432/comercio?user=admin&password=0109&ssl=false";
+    
+    public static Connection getConnection() throws SQLException {
+        System.out.println("Tentando conectar com: " + URL.replace("password=0109", "password="));
+        
         try {
             Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            throw new SQLException("Driver PostgreSQL não encontrado", e);
         }
-    }
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USUARIO, SENHA);
     }
 }
